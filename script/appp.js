e349c2ca787581372e56c4e5171029c20daa2631
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-09-10 02:05:03 +0530

    Feature : Map/Chart [WIP]

--- script/appp.js
+++ script/appp.js
@@ -1,3 +1,118 @@
+// map
+const displayMap = (mapData)=>{
+  const map = L.map("map");
+map.setView([18.66, 74.71], 7);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+for (let i = 0; i < mapData.length; i++) {
+  const d = mapData[i];
+  L.marker([d.reclat, d.reclong], {
+    title: `${d.name}`,
+  })
+    .bindPopup(
+` <span> Name</span>
+<h2>${d.name}</h2>
+<span>Class</span>
+<h2>${d.recclass}</h2>
+<span>Mass (g)</span>
+<h2>${d.mass}</h2>
+<span>Year</span>
+<h2>${d.year}</h2>
+<span>Country</span>
+<h2>Yemen</h2>
+`
+    )
+    .addTo(map);
+}
+L.marker([18.66, 74.71], {
+  title: `india`,
+})
+  .bindPopup(`india sadik 22 `)
+  .addTo(map);
+
+}
+console.log(L);
+
+// bar graph
+// Initialize the year histogram
+const yearHistogram = new Chart(document.getElementById("yearHistogram"), {
+  type: "bar",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Number of Strikes by Year",
+        data: [],
+        backgroundColor: "rgb(225, 85, 33, 0.2)",
+        borderColor: "rgb(225, 85, 33, 1)",
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+function updateChart(results) {
+  console.log(results);
+  const average = document.getElementById("averageStrikes");
+  const total = document.getElementById("totalStrikes");
+
+  const years = results.map((item) =>
+    item.year ? item.year.substring(0, 4) : "Unknown"
+  );
+  const yearCounts = {};
+  years.forEach((year) => (yearCounts[year] = (yearCounts[year] || 0) + 1));
+
+  // Update the year histogram data
+  yearHistogram.data.labels = Object.keys(yearCounts);
+  yearHistogram.data.datasets[0].data = Object.values(yearCounts);
+
+  yearHistogram.update();
+
+  // Calculate and log the average strikes
+  const averageStrikes = calculateAverageStrikes(yearCounts);
+  console.log("Average Strikes per Year:", averageStrikes);
+  average.innerHTML = `${averageStrikes}`;
+  // Calculate and log the total strikes
+  const totalStrikes = calculateTotalStrikes(yearCounts);
+  console.log("Total Strikes:", totalStrikes);
+  total.innerHTML = `${totalStrikes}`;
+}
+
+// Calculate the average number of strikes
+function calculateAverageStrikes(yearCounts) {
+  const totalYears = Object.keys(yearCounts).length;
+  const totalStrikes = Object.values(yearCounts).reduce(
+    (total, count) => total + count,
+    0
+  );
+
+  if (totalYears === 0) {
+    return 0;
+  }
+
+  const averageStrikes = Math.round(totalStrikes / totalYears);
+  return averageStrikes;
+}
+
+// Calculate the total number of strikes
+function calculateTotalStrikes(yearCounts) {
+  const totalStrikes = Object.values(yearCounts).reduce(
+    (total, count) => total + count,
+    0
+  );
+  return totalStrikes;
+}
+
 let tbody = document.querySelector("tbody");
 let pageUL = document.querySelector(".pagination");
 let paginationNumbers = document.querySelector(".pagination1");
@@ -97,6 +212,7 @@ const getPaginationNumber = () => {
 };
 const setCurrentPage = (pageNum, data) => {
   // new code
+  document.getElementById("pageNumber").innerHTML = pageNum;
   if (pageNum == 1) {
     const initial = pageNum;
     const last = pageNum * 10;
@@ -111,7 +227,6 @@ const setCurrentPage = (pageNum, data) => {
   // end here
 
   currentPage = pageNum;
-  handleActivePageNumber();
 
   const prevRange = (pageNum - 1) * pageLimit;
   const currRange = pageNum * pageLimit;
@@ -131,7 +246,7 @@ input.addEventListener("submit", (e) => {
   let inputBox = document.getElementById("inputBox");
 
   if (data != undefined) {
-    data = JSON.parse(data); 
+    data = JSON.parse(data);
     console.log(data.length);
 
     for (const item of data) {
@@ -166,9 +281,7 @@ function Example() {
 fetchData()
   .then((fetchedData) => {
     console.log(fetchedData);
-    getPaginationNumber();
     setCurrentPage(1, fetchData);
-
     preButton.addEventListener("click", () => {
       console.log("previous button has beenn clicked");
       setCurrentPage(currentPage - 1, fetchData);
@@ -180,15 +293,8 @@ fetchData()
       setCurrentPage(currentPage + 1, fetchData);
       console.log("next button has beenn clicked");
     });
-    document.querySelectorAll(".pagination").forEach((button) => {
-      const pageIndex = Number(button.getAttribute("page-index"));
-
-      if (pageIndex) {
-        button.addEventListener("click", () => {
-          setCurrentPage(pageIndex);
-        });
-      }
-    });
+    updateChart(fetchedData);
+    displayMap(fetchedData)
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
